a8b4f9fd80dabe587130950d9e7fe787cd2c4893
Navid <navidchew@gmail.com>
2026-01-21 09:58:10 +0800

    Add skeleton code to greet user and exit

---
+++ src/main/java/Buddy.java
@@ -0,0 +1,10 @@
+public class Buddy {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello I'm Buddy!");
+        System.out.println("What can I do for you?\n");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("____________________________________________________________");
+    }
+}
