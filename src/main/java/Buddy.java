b329efcf7ecca600607f10b76dd60633fdb990a7
Navid <navidchew@gmail.com>
2026-01-21 17:46:34 +0800

    Add Task class to keep track of done and undone tasks

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Buddy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> commands = new ArrayList<>();
+        ArrayList<Task> commands = new ArrayList<>();
         String logo = "    ____  _    _ _____  _______     __\n" +
                 "   |  _ \\| |  | |  __ \\|  __ \\ \\   / /\n" +
                 "   | |_) | |  | | |  | | |  | \\ \\_/ / \n" +
@@ -19,21 +19,36 @@ public class Buddy {
         String userInput = scanner.nextLine();
 
         while (!userInput.equals("bye")) {
-            if (userInput.equals("list")) {
+           if (userInput.equals("list")) {
                 System.out.println("    ____________________________________________________________");
+                System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < commands.size(); i++) {
-                    System.out.println(i+1 + ". " + commands.get(i));
+                    System.out.println("    " + (i+1) + "." + commands.get(i));
                 }
+                System.out.println("    ____________________________________________________________");
+            } else if (userInput.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
+                commands.get(taskIndex).markAsDone();
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("      " + commands.get(taskIndex));
+                System.out.println("    ____________________________________________________________");
+            } else if (userInput.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
+                commands.get(taskIndex).markAsUndone();
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    OK, I've marked this task as not done yet:");
+                System.out.println("      " + commands.get(taskIndex));
                 System.out.println("    ____________________________________________________________");
             } else {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("    added: " + userInput);
                 System.out.println("    ____________________________________________________________");
-                commands.add(userInput);
+                commands.add(new Task(userInput));
             }
             userInput = scanner.nextLine();
         }
-
+        scanner.close();
         System.out.println("    ____________________________________________________________");
         System.out.println("    Bye. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
