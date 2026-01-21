1b3b3da86ee3398dca6a8e09dd627096d0e0e0e1
Navid <navidchew@gmail.com>
2026-01-21 17:17:08 +0800

    Add ArrayList to store user input and display back to user

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Buddy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> commands = new ArrayList<>();
         String logo = "    ____  _    _ _____  _______     __\n" +
                 "   |  _ \\| |  | |  __ \\|  __ \\ \\   / /\n" +
                 "   | |_) | |  | | |  | | |  | \\ \\_/ / \n" +
@@ -17,9 +19,18 @@ public class Buddy {
         String userInput = scanner.nextLine();
 
         while (!userInput.equals("bye")) {
-            System.out.println("    ____________________________________________________________");
-            System.out.println("    " + userInput);
-            System.out.println("    ____________________________________________________________");
+            if (userInput.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < commands.size(); i++) {
+                    System.out.println(i+1 + ". " + commands.get(i));
+                }
+                System.out.println("    ____________________________________________________________");
+            } else {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    added: " + userInput);
+                System.out.println("    ____________________________________________________________");
+                commands.add(userInput);
+            }
             userInput = scanner.nextLine();
         }
 
