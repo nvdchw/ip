d2f48ecedbf9dfdadec3d5da23c15747a8710718
Navid <navidchew@gmail.com>
2026-01-21 16:22:42 +0800

    Add echo user input

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Buddy {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello I'm Buddy!");
-        System.out.println("What can I do for you?\n");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        String logo = "    ____  _    _ _____  _______     __\n" +
+                "   |  _ \\| |  | |  __ \\|  __ \\ \\   / /\n" +
+                "   | |_) | |  | | |  | | |  | \\ \\_/ / \n" +
+                "   |  _ <| |  | | |  | | |  | |\\   /  \n" +
+                "   | |_) | |__| | |__| | |__| | | |   \n" +
+                "   |____/ \\____/|_____/|_____/  |_|   \n";
+        System.out.println(logo);
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello I'm Buddy!");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        String userInput = scanner.nextLine();
+
+        while (!userInput.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + userInput);
+            System.out.println("    ____________________________________________________________");
+            userInput = scanner.nextLine();
+        }
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
