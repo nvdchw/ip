6986e740e435e1bc8c1ef13dbfa432958b55fc0a
Navid <navidchew@gmail.com>
2026-01-22 11:13:32 +0800

    Add Todo.java Deadline.java and Event.java to handle different types of Tasks

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -19,7 +19,7 @@ public class Buddy {
         String userInput = scanner.nextLine();
 
         while (!userInput.equals("bye")) {
-           if (userInput.equals("list")) {
+            if (userInput.equals("list")) {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < commands.size(); i++) {
@@ -40,11 +40,41 @@ public class Buddy {
                 System.out.println("    OK, I've marked this task as not done yet:");
                 System.out.println("      " + commands.get(taskIndex));
                 System.out.println("    ____________________________________________________________");
-            } else {
+            } else if (userInput.startsWith("todo ")) {
+                String description = userInput.substring(5);
+                Task task = new Todo(description);
+                commands.add(task);
                 System.out.println("    ____________________________________________________________");
-                System.out.println("    added: " + userInput);
+                System.out.println("    Got it. I've added this task:");
+                System.out.println("      " + task);
+                System.out.println("    Now you have " + commands.size() + " tasks in the list.");
                 System.out.println("    ____________________________________________________________");
-                commands.add(new Task(userInput));
+            } else if (userInput.startsWith("deadline ")) {
+                String content = userInput.substring(9);
+                int byIndex = content.indexOf(" /by ");
+                String description = content.substring(0, byIndex);
+                String by = content.substring(byIndex + 5);
+                Task task = new Deadline(description, by);
+                commands.add(task);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Got it. I've added this task:");
+                System.out.println("      " + task);
+                System.out.println("    Now you have " + commands.size() + " tasks in the list.");
+                System.out.println("    ____________________________________________________________");
+            } else if (userInput.startsWith("event ")) {
+                String content = userInput.substring(6);
+                int fromIndex = content.indexOf(" /from ");
+                int toIndex = content.indexOf(" /to ");
+                String description = content.substring(0, fromIndex);
+                String from = content.substring(fromIndex + 7, toIndex);
+                String to = content.substring(toIndex + 5);
+                Task task = new Event(description, from, to);
+                commands.add(task);
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Got it. I've added this task:");
+                System.out.println("      " + task);
+                System.out.println("    Now you have " + commands.size() + " tasks in the list.");
+                System.out.println("    ____________________________________________________________");
             }
             userInput = scanner.nextLine();
         }
